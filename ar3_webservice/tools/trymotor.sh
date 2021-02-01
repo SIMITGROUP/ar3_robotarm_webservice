27c27c75a6a21b7be267945b00cb9c083008fb73
kstan <kstan@simitgroup.com>
2021-02-01 13:43:28 +0800

    add test script

---
+++ ar3_webservice/tools/trymotor.sh
@@ -0,0 +1,41 @@
+#/bin/bash
+curl http://127.0.0.1:5000/servo/mygripper?degree=90
+sleep 2
+curl http://127.0.0.1:5000/servo/mygripper?degree=120
+sleep 2
+curl http://127.0.0.1:5000/servo/mygripper?degree=140
+sleep 2
+curl http://127.0.0.1:5000/servo/mygripper?degree=90
+sleep 2
+
+curl http://127.0.0.1:5000//move_j/j1?degree=-10
+curl http://127.0.0.1:5000//move_j/j1?degree=10
+curl http://127.0.0.1:5000//move_j/j1?degree=-20
+curl http://127.0.0.1:5000//move_j/j1?degree=20
+
+
+curl http://127.0.0.1:5000//move_j/j2?degree=10
+curl http://127.0.0.1:5000//move_j/j2?degree=-10
+curl http://127.0.0.1:5000//move_j/j2?degree=20
+curl http://127.0.0.1:5000//move_j/j2?degree=-20
+
+
+curl http://127.0.0.1:5000//move_j/j3?degree=10
+curl http://127.0.0.1:5000//move_j/j3?degree=-10
+curl http://127.0.0.1:5000//move_j/j3?degree=20
+curl http://127.0.0.1:5000//move_j/j3?degree=-20
+
+curl http://127.0.0.1:5000//move_j/j4?degree=10
+curl http://127.0.0.1:5000//move_j/j4?degree=-10
+curl http://127.0.0.1:5000//move_j/j4?degree=20
+curl http://127.0.0.1:5000//move_j/j4?degree=-20
+
+curl http://127.0.0.1:5000//move_j/j5?degree=10
+curl http://127.0.0.1:5000//move_j/j5?degree=-10
+curl http://127.0.0.1:5000//move_j/j5?degree=20
+curl http://127.0.0.1:5000//move_j/j5?degree=-20
+
+curl http://127.0.0.1:5000//move_j/j6?degree=10
+curl http://127.0.0.1:5000//move_j/j6?degree=-10
+curl http://127.0.0.1:5000//move_j/j6?degree=20
+curl http://127.0.0.1:5000//move_j/j6?degree=-20
